1d1b546f85bbeeb3b164eb63826f27f7acb06295
Rodeni4 <rodenkosv@gmail.com>
2023-01-26 23:55:59 +0700

    Add class BunTest

---
+++ src/test/java/praktikum/BunTest.java
@@ -0,0 +1,14 @@
+package praktikum;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BunTest {
+
+    @org.junit.jupiter.api.Test
+    void getName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getPrice() {
+    }
+}
